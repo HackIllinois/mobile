cb58ada2f161496dc0d477d51c818e0a47c74d5d
yashjagtap23 <108370176+yashjagtap23@users.noreply.github.com>
2025-10-12 16:46:11 -0500

    Lofi animation
    Lofi animation on app startup

--- App.tsx
+++ App.tsx
@@ -1,13 +1,12 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import StartupAnimation from './src/components/hackrocket/StartupAnimation';
+
+
+
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+  return <StartupAnimation />;
 }
 
 const styles = StyleSheet.create({
